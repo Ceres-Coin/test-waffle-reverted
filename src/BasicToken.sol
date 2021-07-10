991d87b82774239869c074dde534c6bc463837ed
IlyaPrigogine <prigogine@protonmail.com>
2021-07-10 11:35:44 +0800

    submitted

--- src/BasicToken.sol
+++ src/BasicToken.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Example class - a mock class using delivering from ERC20
 contract BasicToken is ERC20 {
-  constructor(uint256 initialBalance) ERC20("Basic", "BSC") public {
+  constructor(uint256 initialBalance) ERC20("Basic", "BSC")  {
       _mint(msg.sender, initialBalance);
   }
 }
